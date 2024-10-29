ef3c6709b773e7d33b9b750832cce6da9dae8c25
Daquarter <135808166+2303110264@users.noreply.github.com>
2024-10-29 11:19:45 +0900

    Update mySTVisualizer.py

--- mySTVisualizer.py
+++ mySTVisualizer.py
@@ -9,7 +9,8 @@ def is_running_on_localserver():
 
     #return "STREAMLIT_SERVER_PORT" in os.environ
     
-    server_address = os.environ.get("SERVER_NAME", "localhost")
+    #server_address = os.environ.get("SERVER_NAME", "localhost")
+    server_address = st.server.server_address[0]
     print(server_address)
     if server_address not in ["localhost"]:
         return False
