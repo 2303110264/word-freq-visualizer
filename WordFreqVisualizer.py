0bac991129750040f8cc8aaa4f6440296ed7980e
Daquarter <135808166+2303110264@users.noreply.github.com>
2024-10-31 13:46:38 +0900

    Update WordFreqVisualizer.py

--- WordFreqVisualizer.py
+++ WordFreqVisualizer.py
@@ -23,9 +23,9 @@ st.set_page_config(
 
 @st.dialog("분석할 데이터 확인")
 def view_raw_data(data_file):
-    df = pd.read_csv(data_file)
-    head_num = st.slider('단어수', 1, len(df), 10, 1)
-    st.write(df.head(head_num))
+    st.session_state.df = pd.read_csv(data_file)
+    head_num = st.slider('출력 라인 수', 1, len(st.session_state.df), 10, 1)
+    st.write(st.session_state.df.head(head_num))
     
 with st.sidebar:    
     st.write('## 영화 키워드 분석 메뉴')
