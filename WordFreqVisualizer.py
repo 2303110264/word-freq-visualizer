60f77a1f0917a6138388494eea032437985b7cbd
Daquarter <135808166+2303110264@users.noreply.github.com>
2024-10-31 13:54:46 +0900

    Update WordFreqVisualizer.py

--- WordFreqVisualizer.py
+++ WordFreqVisualizer.py
@@ -22,7 +22,7 @@ st.set_page_config(
 )
 
 @st.dialog("분석할 데이터 확인")
-def view_raw_data(st.session_state.data_file):
+def view_raw_data():
     df = pd.read_csv(st.session_state.data_file)
     st.write(df.info())
     head_num = st.slider('출력 라인 수', 1, 100, 10, 1)
@@ -30,10 +30,10 @@ def view_raw_data(st.session_state.data_file):
     
 with st.sidebar:    
     st.write('## 영화 키워드 분석 메뉴')
-    filename = st.file_uploader("분석할 파일", type=['csv'])
-    if filename:
+    st.session_state.filename = st.file_uploader("분석할 파일", type=['csv'])
+    if st.session_state.filename:
         if st.button("분석할 데이터 보기"):
-            view_raw_data(filename)
+            view_raw_data()
         col = st.text_input("분석할 컬럼")
         with st.form('my_form'):
             freq = st.checkbox('빈도수 그래프')
