44ee5c103bc3cf662bd6272901a1d0957ae506d4
Daquarter <135808166+2303110264@users.noreply.github.com>
2024-10-31 13:32:05 +0900

    Update mySTVisualizer.py

--- mySTVisualizer.py
+++ mySTVisualizer.py
@@ -19,18 +19,20 @@ def is_running_on_localserver():
 @st.cache_data
 def visualize_barhgraph(counter, num_words):
 
-    if is_running_on_localserver():
+    #if is_running_on_localserver():
         #font_path = "c:/Windows/Fonts/malgun.ttf"
-        font_path = "./Hakgyoansim Badasseugi TTF L.ttf"
+        #font_path = "./Hakgyoansim Badasseugi TTF L.ttf"
         #font_name = font_manager.FontProperties(fname=font_path).get_name() 
-        font_name = "Hakgyoansim Badasseugi TTF L"
-        font_manager.fontManager.addfont(font_path)
-    else:
-        font_path = '/usr/share/fonts/truetype/nanum/NanumGothic.ttf'
-        font_name = 'NanumGothic'
-        font_manager.fontManager.addfont(font_path)
+        #font_name = "Hakgyoansim Badasseugi TTF L"
+        #font_manager.fontManager.addfont(font_path)
+    #else:
+        #font_path = '/usr/share/fonts/truetype/nanum/NanumGothic.ttf'
+        #font_name = 'NanumGothic'
+        #font_manager.fontManager.addfont(font_path)
         
-
+    font_path = "./Hakgyoansim Badasseugi TTF L.ttf"
+    font_name = font_manager.FontProperties(fname=font_path).get_name()
+    font_manager.fontManager.addfont(font_path)
     rc('font', family=font_name)
 
     word_list = [word for word, _ in counter.most_common(num_words)]
